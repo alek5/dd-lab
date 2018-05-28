89b01d4c435d1003b40d96d1496d5fcc5bc9219f
Alek <uitaleks@gmail.com>
2018-05-28 14:18:11 +0400

    lab-2 filtered address book

--- lab-2/webpack.config.js
+++ lab-2/webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const config = {
-	devtool: true,
 	watch: true,
 
     context: path.resolve(__dirname, 'src'),
